31083af884beb5e56f4dbf76f0a706da7458da2d
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-02-11 02:08:00 +0700

    add css add navbar

--- django project/aps_ce_frontend/src/App.js
+++ django project/aps_ce_frontend/src/App.js
@@ -6,10 +6,15 @@ import ReqAna from './Pages/reqAna';
 import GenModel from './Pages/genModel';
 import ModelList from './Pages/modelList';
 import CareerUpdate from './Pages/updateStudentCareer';
+import OutputCareer from './Pages/outputCareer';
+import Navbar from './Component/navbar';
+import { theme } from './Component/theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 function App() {
     return (
-        <>
+        <ThemeProvider theme={theme}>
+        <Navbar/>
         <Routes>
             <Route path="/" element={<center><HomePage/></center>} />
             <Route path="/subjectUpload" element={<center><SubjectUpload/></center>} />
@@ -17,8 +22,9 @@ function App() {
             <Route path="/genModel" element={<center><GenModel/></center>} />
             <Route path="/temp" element={<center><ModelList/></center>} />
             <Route path="/careerUpdate" element={<center><CareerUpdate/></center>} />
+            <Route path="/outputCareer" element={<center><OutputCareer/></center>} />
         </Routes>
-        </>
+        </ThemeProvider>
     );
 }
 
---
+++ django project/aps_ce_frontend/src/Pages/outputCareer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import axios from 'axios';
+import Navbar from '../Component/navbar';
+
+const OutputCareer = () => {
+    return(
+        <h1>career</h1>
+    );
+};
+
+export default OutputCareer;
