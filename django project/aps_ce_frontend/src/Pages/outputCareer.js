ada8951e911882f421e826b6c0e976fd62526ca8
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-02-15 18:03:05 +0700

    อัพหน้า outputcareer

--- django project/aps_ce_frontend/src/Pages/outputCareer.js
+++ django project/aps_ce_frontend/src/Pages/outputCareer.js
@@ -4,31 +4,75 @@ import axios from 'axios';
 
 function OutputCareer() {
   const [careerData, setCareerData] = useState({});
+  const [outputType, setOutputType] = useState("staticResult");
+  const [selectedYear, setSelectedYear] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:8000/getCareerResult')
+    if(outputType == "staticResult"){
+      axios.get("http://localhost:8000/getCareerResult/")
       .then(response => {
         setCareerData(response.data);
       })
       .catch(error => {
         console.log(error);
       });
-  }, []);
+    }
+    else if(outputType == "staticPredictResult"){
+      axios.get("http://localhost:8000/getCareerResult/")
+      .then(response => {
+        setCareerData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  }, [outputType]);
 
-  console.log(careerData);
+  const handleYearChange = (event) => {
+    setSelectedYear(event.target.value);
+  }
+
+  const filteredCareerData = Object.entries(careerData).reduce((acc, [career, data]) => {
+    if (selectedYear === "" || data.Year === selectedYear) {
+      acc[career] = data;
+    }
+    return acc;
+  }, {});
+
+  const handleButtonClick = (type) => {
+    setOutputType(type);
+    setSelectedYear("");
+  }
 
   return (
-    <div>
-      <h1>Career Result</h1>
-      <ul>
-        {Object.keys(careerData).map((career, index) => (
-          <li key={index}>
-            {career}: {careerData[career].Num_of_student}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <button onClick={() => handleButtonClick("staticResult")}>Static Result</button>
+        <button onClick={() => handleButtonClick("staticPredictResult")}>Static Predict Result</button>
+      </div>
+      {outputType == 'staticResult'?
+        <div>
+          <h1>Career Result</h1>
+          <div>
+            <label htmlFor="year-select">Select a year:</label>
+            <select id="year-select" value={selectedYear} onChange={handleYearChange}>
+              <option value="">All years</option>
+              <option value="2561">2561</option>
+              <option value="2560">2560</option>
+            </select>
+          </div>
+          <ul>
+            {Object.keys(filteredCareerData).map((career, index) => (
+              <li key={index}>
+                {career}: {filteredCareerData[career].Num_of_student}
+              </li>
+            ))}
+          </ul>
+        </div>
+      :<div></div>}
+    </>
   );
 }
 
 export default OutputCareer;
+
