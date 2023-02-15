4ff41db11c8b4dc7afd515a5248f39c1ff7f4851
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-02-15 18:19:50 +0700

    แก้ให้ footer อยู่ล่างสุด

--- django project/aps_ce_frontend/src/Component/footer.js
+++ django project/aps_ce_frontend/src/Component/footer.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
-    <footer style={{ backgroundColor: '#FB8500', color: 'white' ,textAlign: 'center'}}>
+    <footer
+      style={{
+        backgroundColor: "#FB8500",
+        color: "white",
+        textAlign: "center",
+        position: "fixed",
+        bottom: "0px",
+        left: "0px",
+        right: "0px"
+      }}
+    >
       Copyright &copy; {new Date().getFullYear()} Your Company
     </footer>
   );
