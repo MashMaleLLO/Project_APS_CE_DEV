50ed3e98cef7ac478a11e43f5eec195c1b49b0c9
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-02-21 18:08:56 +0700

    อัพหน้า predict

--- django project/aps_ce_frontend/src/Pages/predictStudent.js
+++ django project/aps_ce_frontend/src/Pages/predictStudent.js
@@ -16,7 +16,7 @@ const PredictStudent = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target.model.value)
+    console.log(e.target.model.value);
     async function fetchData() {
       let res = await axios.post("http://localhost:8000/reqPredict", formData);
       console.log(res.data);
@@ -29,7 +29,11 @@ const PredictStudent = () => {
     e.preventDefault();
     async function getfile() {
       let res = await axios({
-        url: "http://localhost:8000/reqAna/" + e.target.curri.value + "/" + e.target.year.value, //your url
+        url:
+          "http://localhost:8000/reqAna/" +
+          e.target.curri.value +
+          "/" +
+          e.target.year.value, //your url
         method: "GET",
         responseType: "blob", // important
       }).then((response) => {
@@ -45,30 +49,40 @@ const PredictStudent = () => {
   };
 
   return (
-    <div className="flex flex-col gap-6 justify-center items-center h-screen">
-      <h1>Upload your grade CSV (Save file as CSV-UTF-8)</h1>
-      <form onSubmit={handleSubmit}>
-        {/* <select name='pred'>
+    <div className="container mx-auto py-8 px-4">
+      <h1 className="py-4 text-xl md:text-2xl font-bold">พยากรณ์นักศึกษา</h1>
+      <div className="container mx-auto flex flex-col rounded-lg drop-shadow-md bg-white px-8 py-8 space-y-6 w-full h-[500px]">
+        <div className="flex flex-wrap items-end gap-8">
+          <div className="space-y-2 text-lg md:text-2xl w-full md:w-1/4">
+            <form onSubmit={handleSubmit}>
+              {/* <select name='pred'>
           <option value="Grade">Predict By Grade</option>
           <option value="Class">Predict By Class</option>
         </select> */}
-        <input type="file" accept=".csv" onChange={handleChange} />
-        <button
-          type="submit"
-          className="bg-blue-500 px-4 py-2 rounded-md font-semibold"
-        >
-          fetch
-        </button>
-      </form>
-      <br></br>
-      <form onSubmit={getDownloadFile}>
-        <select name='curri'>
-          <option value="computer">วิศวกรรมคอมพิวเตอร์</option>
-          <option value="computerNext">วิศวกรรมคอมพิวเตอร์ (ต่อเนื่อง)</option>
-        </select>
-        <YearList></YearList>
-        <button type="submit">Download CSV Format For subject 2560</button>
-      </form>
+              <input type="file" accept=".csv" onChange={handleChange} />
+              <button
+                type="submit"
+                className="bg-blue-500 px-4 py-2 rounded-md font-semibold"
+              >
+                fetch
+              </button>
+            </form>
+            <br></br>
+            <form onSubmit={getDownloadFile}>
+              <select name="curri">
+                <option value="computer">วิศวกรรมคอมพิวเตอร์</option>
+                <option value="computerNext">
+                  วิศวกรรมคอมพิวเตอร์ (ต่อเนื่อง)
+                </option>
+              </select>
+              <YearList></YearList>
+              <button type="submit">
+                Download CSV Format For subject 2560
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
