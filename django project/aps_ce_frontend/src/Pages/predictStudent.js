c37fb1ee4bf83648b87c3ce30715953c9c0209e4
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-02-21 20:00:58 +0700

    เพิ่มไฟล์ tailwind อัพหน้า predict

--- django project/aps_ce_frontend/src/Pages/predictStudent.js
+++ django project/aps_ce_frontend/src/Pages/predictStudent.js
@@ -52,36 +52,63 @@ const PredictStudent = () => {
     <div className="container mx-auto py-8 px-4">
       <h1 className="py-4 text-xl md:text-2xl font-bold">พยากรณ์นักศึกษา</h1>
       <div className="container mx-auto flex flex-col rounded-lg drop-shadow-md bg-white px-8 py-8 space-y-6 w-full h-[500px]">
-        <div className="flex flex-wrap items-end gap-8">
-          <div className="space-y-2 text-lg md:text-2xl w-full md:w-1/4">
-            <form onSubmit={handleSubmit}>
-              {/* <select name='pred'>
-          <option value="Grade">Predict By Grade</option>
-          <option value="Class">Predict By Class</option>
-        </select> */}
-              <input type="file" accept=".csv" onChange={handleChange} />
+        <form onSubmit={getDownloadFile}>
+          <div className="flex flex-wrap items-end gap-8">
+            <div className="space-y-2 text-lg md:text-2xl w-full md:w-1/4">
+              {/* เเถวที่ 1 : Dropdown*/}
+
+              <label htmlFor="select-option">คณะ</label>
+              <select
+                id="select-option"
+                className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
+              >
+                <option value="computer">วิศวกรรมคอมพิวเตอร์</option>
+                <option value="computerNext">วิศวกรรมไฟฟ้า</option>
+              </select>
+            </div>
+            <div className="space-y-2 text-lg md:text-2xl w-full md:w-1/4">
+              <label htmlFor="select-option">หลักสูตร</label>
+              <select
+                id="select-option"
+                className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
+              >
+                <option value="computer">ปกติ</option>
+                <option value="computerNext">ต่อเนื่อง</option>
+              </select>
+            </div>
+            <div className=" text-lg md:text-2xl w-full md:w-2/4">
+              <button
+                type="download"
+                className="text-white text-text-lg md:text-2xl px-4 py-2 rounded-lg bg-[#FF9D2E] hover:bg-[#F28204]"
+              >
+                ดาวน์โหลดไฟล์แบบฟอร์มสำหรับการวิเคราะห์
+              </button>
+            </div>
+          </div>
+        </form>
+
+        {/* เเถวที่ 2 : uplaod file + พยากรณ์*/}
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-wrap gap-8">
+            <div className="w-full md:w-1/4 ">
+              <input
+                type="file"
+                accept=".csv"
+                onChange={handleChange}
+                className="w-full px-4 py-1 text-gray-500 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none
+              file:bg-[#FF9D2E] file:rounded-lg file:border-none file:px-4 file:py-2 file:text-white file:cursor-pointer file:mr-4"
+              />
+            </div>
+            <div>
               <button
                 type="submit"
-                className="bg-blue-500 px-4 py-2 rounded-md font-semibold"
+                className="text-white text-lg md:text-2xl px-4 py-2 rounded-lg bg-[#FB8500] hover:bg-[#F28204]"
               >
-                fetch
+                พยากรณ์
               </button>
-            </form>
-            <br></br>
-            <form onSubmit={getDownloadFile}>
-              <select name="curri">
-                <option value="computer">วิศวกรรมคอมพิวเตอร์</option>
-                <option value="computerNext">
-                  วิศวกรรมคอมพิวเตอร์ (ต่อเนื่อง)
-                </option>
-              </select>
-              <YearList></YearList>
-              <button type="submit">
-                Download CSV Format For subject 2560
-              </button>
-            </form>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
