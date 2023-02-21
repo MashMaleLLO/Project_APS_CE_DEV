889d432ccff6c25b93442b1c2daafa74c81a93fc
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-02-21 16:21:46 +0700

    อัพเดตหน้าฟร้อน login

--- django project/aps_ce_frontend/src/Component/footer.js
+++ django project/aps_ce_frontend/src/Component/footer.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-function Footer() {
+const Footer = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="relative w-full px-8 py-8 bg-[#FB8500] position:fixed bottom-0 left-0 right-0;">
+      <div className="flex justify-between space-x-4">
+        <span className="text-lg md:text-xl text-white">
+          ระบบคาดการณ์ผลลัพธ์การผลิตบัณฑิตของหลักสูตรจากข้อมูลผลการเรียนของนักศึกษา{" "}
+          <br />
+          Curriculum output prediction from student academic data
+        </span>
+
+        <div className="flex flex-col text-white text-lg md:text-xl">
+          <span>อาจารย์ที่ปรึกษา ผศ.ดร.ธนัญชัย ตรีภาค</span>
+          <span>ผู้จัดทำ นางสาว ณิชกานต์ สุขุมจิตพิทโยทัย</span>
+          <span className="ml-1 indent-16"> นาย นรวิชญ์ อยู่บัว</span>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Footer;
--- django project/aps_ce_frontend/src/Pages/login.js
+++ django project/aps_ce_frontend/src/Pages/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import {createRoot} from 'react-dom/client';
+import { useNavigate } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -9,7 +9,7 @@ const Login = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
-  const rootElement = document.getElementById('root');
+  const rootElement = document.getElementById("root");
   const root = createRoot(rootElement);
 
   const handleSubmit = async (event) => {
@@ -41,33 +41,45 @@ const Login = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label>
-          <b>Username:</b>
-          <input
-            type="text"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-        <br />
-        <br />
-        <label>
-          <b>Password:</b>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-        <br />
-        <br />
-        <button type="submit">Login</button>
-        <br />
-        <br />
+        <div className="">
+          <div className="container mx-auto flex items-center justify-center rounded-lg drop-shadow-md bg-white px-2 py-8 w-full md:w-[550px]">
+            <div className="flex flex-col gap-4">
+              <h1 className="mb-4 font-bold text-xl md:text-3xl text-center">
+                เข้าสู่ระบบ
+              </h1>
+              <div className="space-y-2">
+                <span className="text-lg md:text-2xl">ชื่อผู้ใช้งาน</span>
+                <input
+                  type="username"
+                  value={username}
+                  className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              </div>
+
+              <div className="space-y-2">
+                <span className="text-lg md:text-2xl">รหัสผ่าน</span>
+                <input
+                  type="password"
+                  value={password}
+                  className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <div className="text-center py-6">
+                  <button
+                    type="submit"
+                    className="text-white text-lg md:text-2xl px-6 py-2 rounded-lg bg-[#FB8500] hover:bg-[#F28204]"
+                  >
+                    เข้าสู่ระบบ
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
       </form>
       {localStorage.getItem("isLoggedIn") === "true" && (
-        
         <div>
           <button onClick={handleLogout}>Logout</button>
         </div>
