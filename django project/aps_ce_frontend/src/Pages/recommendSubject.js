706c0926c14e523bd21ad430ffd0ed11650a0254
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-02-26 22:22:33 +0700

    อัพหน้า Recommend Subject

--- django project/aps_ce_frontend/src/Pages/recommendSubject.js
+++ django project/aps_ce_frontend/src/Pages/recommendSubject.js
@@ -3,58 +3,83 @@ import React from "react";
 import axios from "axios";
 
 const RecommendSubject = () => {
-  const [year, setYear] = useState("2564"); // default value for year is 2564
+  const [year, setYear] = useState("2564");
+  const [subjects, setSubjects] = useState([]);
+  const [selectedSubjects, setSelectedSubjects] = useState([]);
 
   const handleYearChange = (e) => {
-    setYear(e.target.value); // update the year state when the user selects a different year
+    setYear(e.target.value);
+  };
+
+  const handleCheckboxChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+    if (isChecked) {
+      setSelectedSubjects([...selectedSubjects, value]);
+    } else {
+      setSelectedSubjects(selectedSubjects.filter((s) => s !== value));
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await axios.post("http://localhost:8000/recommendSubject", {
+      key: selectedSubjects,
+      year: year,
+    });
+    setSubjects(result.data);
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("http://localhost:8000/getkeysubject", {
-        year: year, // send the selected year in the request body as a JSON object
+      const result = await axios.post("http://localhost:8000/keysubject", {
+        year: year,
       });
-      console.log(result.data); // log the response from the API
-      // TODO: update your UI based on the response data
+      setSubjects(result.data);
     };
     fetchData();
-  }, [year]); // re-run the effect when the year state changes
+  }, [year]);
 
   return (
-    <div className="flex flex-col min-h-screen justify-between">
-      <div className="container mx-auto py-8 px-4">
-        <h1 className="py-12 text-xl md:text-2xl font-bold">
-          แนะนำวิชาเลือกภาค
-        </h1>
-        <div className="container mx-auto flex flex-col rounded-lg drop-shadow-md bg-white px-8 py-8 space-y-6 w-full h-[500px]">
-          <div className="flex flex-wrap items-end gap-8">
-            <div className="space-y-2 text-base md:text-lg w-full md:w-1/4">
-              {/* เเถวที่ 1 : Dropdown*/}
-              <label htmlFor="select-option">คณะ</label>
-              <select
-                id="select-option"
-                className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
-              >
-                <option value="computer">วิศวกรรมคอมพิวเตอร์</option>
-                <option value="computerNext">วิศวกรรมคอมพิวเตอร์(ต่อเนื่อง)</option>
-              </select>
+    <>
+      <h1>แนะนำวิชาเลือกภาค</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="select-faculty">คณะ</label>
+        <select
+          id="select-faculty"
+          className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
+        >
+          <option value="computer">วิศวกรรมคอมพิวเตอร์</option>
+          <option value="computerNext">วิศวกรรมคอมพิวเตอร์(ต่อเนื่อง)</option>
+        </select>
+        <label htmlFor="select-year">หลักสูตร</label>
+        <select
+          id="select-year"
+          className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
+          onChange={handleYearChange}
+          value={year}
+        >
+          <option value="2560">2560</option>
+          <option value="2564">2564</option>
+        </select>
+        <div>
+          {subjects.map((subject) => (
+            <div key={subject}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={subject}
+                  checked={selectedSubjects.includes(subject)}
+                  onChange={handleCheckboxChange}
+                />
+                {subject}
+              </label>
             </div>
-            <div className="space-y-2 text-base md:text-lg w-full md:w-1/4">
-              <label htmlFor="select-option">หลักสูตร</label>
-              <select
-                id="select-option"
-                className="w-full px-4 py-2 bg-white border border-grey-300 rounded-lg focus:bg-grey-200 focus:border-[#FB8500] focus:outline-none"
-                onChange={handleYearChange} // call handleYearChange when the user selects a different year
-                value={year} // set the selected year as the value of the dropdown
-              >
-                <option value="2560">2560</option>
-                <option value="2564">2564</option>
-              </select>
-            </div>
-          </div>
+          ))}
         </div>
-      </div>
-    </div>
+        <button type="submit">แนะนำวิชาเลือกภาค</button>
+      </form>
+    </>
   );
 };
 
