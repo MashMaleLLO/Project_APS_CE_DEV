2b94ed2ba2a032bf0530d5aeb675e741feba7b13
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-03-07 22:02:38 +0700

    อัพหน้า Data upload อัพโหลดได้

--- django project/aps_ce_frontend/src/Pages/dataUpload.js
+++ django project/aps_ce_frontend/src/Pages/dataUpload.js
@@ -1,10 +1,21 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const DataUpload = () => {
   let formData = new FormData();
   const [csvFile, setCsvFile] = useState();
-  const [dataType, setDataType] = useState("subjectData");
+  const [dataType, setDataType] = useState("ข้อมูลเกรดและนักศึกษา");
+  const [data, setData] = useState([]);
+  const [pageStudentData, setPageStudentData] = useState(false);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get("http://localhost:8000/getFile");
+      setData(response.data.message);
+    }
+    fetchData();
+  }, []);
 
   if (csvFile) {
     formData.append("path_to_csv", csvFile);
@@ -16,64 +27,106 @@ const DataUpload = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    async function fetchData() {
-      let res = await axios.post("http://localhost:8000/fileUpload", formData);
-      console.log(res.data);
-    }
+    formData.append("type_data", JSON.stringify(dataType)); // append the JSON object to the form data
+    axios
+      .post("http://localhost:8000/fileUpload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data", // set the content type to multipart/form-data
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  const handleDelete = (e, id) => {
+    e.preventDefault();
+    async function fetchData() {
+      const response = await axios.delete(
+        `http://localhost:8000/getFile?id=${id}`
+      );
+    }
     fetchData();
   };
 
   useEffect(() => {
-    if (dataType == "subjectData") {
-      let res = axios.post("http://localhost:8000/getCareerResult/", formData);
-    } else if (dataType == "studentData") {
-      let res = axios.post("http://localhost:8000/getCareerResult/", formData);
+    if (dataType === "ข้อมูลรายวิชา") {
+      setFilteredData(
+        data.filter((item) => item.type_data === "ข้อมูลรายวิชา")
+      );
+      setPageStudentData(false);
+    } else if (dataType === "ข้อมูลเกรดและนักศึกษา") {
+      setFilteredData(
+        data.filter((item) => item.type_data === "ข้อมูลเกรดและนักศึกษา")
+      );
+      setPageStudentData(true);
     }
-  }, [dataType]);
+    console.log("หลังฟิล" + filteredData);
+  }, [dataType, data]);
 
   const handleButtonClick = (type) => {
     setDataType(type);
-  }
-
-  console.log("nun");
+  };
 
   return (
     <>
       <div>
-        <button onClick={() => handleButtonClick("subjectData")}>
+        <button onClick={() => handleButtonClick("ข้อมูลรายวิชา")}>
           Subject Data
         </button>
         <br></br>
-        <button onClick={() => handleButtonClick("studentData")}>
+        <button onClick={() => handleButtonClick("ข้อมูลเกรดและนักศึกษา")}>
           Student Data
         </button>
       </div>
-      {dataType == "subjectData" ? (
+      {pageStudentData ? (
         <div className="flex flex-col gap-6 justify-center items-center h-screen">
-          <h1>File Subject Data Uploader</h1>
+          <h1>ข้อมูลในระบบ</h1>
           <form onSubmit={handleSubmit}>
             <input type="file" accept=".csv" onChange={handleChange} />
             <button
               type="submit"
               className="bg-blue-500 px-4 py-2 rounded-md font-semibold"
             >
-              fetch
+              อัพโหลดไฟล์
             </button>
           </form>
+          <ul>
+            {filteredData.map((item) => (
+              <div key={item.id}>
+                <p>{item.name}</p>
+                <button onClick={(e) => handleDelete(e, item.id)}>
+                  Delete
+                </button>
+              </div>
+            ))}
+          </ul>
         </div>
       ) : (
         <div className="flex flex-col gap-6 justify-center items-center h-screen">
-          <h1>File Student Data Uploader</h1>
+          <h1>ข้อมูลในระบบ</h1>
           <form onSubmit={handleSubmit}>
             <input type="file" accept=".csv" onChange={handleChange} />
             <button
               type="submit"
               className="bg-blue-500 px-4 py-2 rounded-md font-semibold"
             >
-              fetch
+              อัพโหลดไฟล์
             </button>
           </form>
+          <ul>
+            {filteredData.map((item) => (
+              <div key={item.id}>
+                <p>{item.name}</p>
+                <button onClick={(e) => handleDelete(e, item.id)}>
+                  Delete
+                </button>
+              </div>
+            ))}
+          </ul>
         </div>
       )}
     </>
