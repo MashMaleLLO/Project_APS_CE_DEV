8663c934117bdb050388702c45dac7d6d989baa6
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2023-03-12 17:34:16 +0700

    อัพหน้าfront

--- django project/aps_ce_frontend/src/Pages/dataEdit.js
+++ django project/aps_ce_frontend/src/Pages/dataEdit.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate , useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DataEdit = () => {
   const navigate = useNavigate();
-  const {id} = useParams();
+  const { id } = useParams();
   const [data, setData] = useState([]);
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [editContent, setEditContent] = useState("");
 
   const dataUpload = () => {
     let dataUpload = `/dataUpload`;
@@ -14,21 +16,69 @@ const DataEdit = () => {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(`http://localhost:8000/getFile/`+id);
+      const response = await axios.get(`http://localhost:8000/getFile/` + id);
       setData(response.data.message.file_content);
     }
     fetchData();
   }, []);
 
-  console.log('ควย',data);
+  const handleRowClick = (row) => {
+    setSelectedRow(row);
+  };
+
+  const handleEditFormSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log("index",data.indexOf(selectedRow));
+    console.log("content",editContent);
+    const response = await axios.put(`http://localhost:8000/editFileContent/${id}`, {
+      action: "Edit",
+      index: data.indexOf(selectedRow),
+      content: editContent,
+    });
+
+    setData(response.data.message.file_content);
+    setSelectedRow(null);
+    setEditContent("");
+  };
 
   return (
     <>
-      <h1>สวัสดี</h1>
+      <table>
+        <thead>
+          <tr>
+            {data.length > 0 &&
+              Object.keys(data[0]).map((key) => <th key={key}>{key}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index} onClick={() => handleRowClick(item)}>
+              {Object.keys(item).map((key) => (
+                <td key={key}>{item[key]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {selectedRow && (
+        <div>
+          <h2>Row Details</h2>
+          <p>{JSON.stringify(selectedRow)}</p>
+          <form onSubmit={handleEditFormSubmit}>
+            <label>
+              Edit content:
+              <input
+                type="text"
+                value={editContent}
+                onChange={(event) => setEditContent(event.target.value)}
+              />
+            </label>
+            <button type="submit">Save</button>
+          </form>
+        </div>
+      )}
       <button onClick={dataUpload}>กลับ</button>
-      {data.map((item) => (
-                <p>{item.student_id}</p>
-            ))}
     </>
   );
 };
